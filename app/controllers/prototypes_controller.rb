7fed0ce7fea567528d5f302b0f3925cecd156c16
Sedwardy <101158263+Sedwardy@users.noreply.github.com>
2022-04-01 16:00:35 +0900

    initialcommit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,9 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user! ,only: [:new, :edit, :delete]
+  
+  
   def index
+    @prototypes = Prototype.all
   end
 
   def new
@@ -14,9 +18,36 @@ class PrototypesController < ApplicationController
       render :new
     end
   end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+     if @prototype.update(message_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
   
 private
-
   def message_params
     params.require(:prototype).permit( :title, :catch_copy, :concept, :image ).merge(user_id: current_user.id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :prototypes, only: [ :index, :new, :create ]
+  resources :prototypes do
+    resources :comments, only: :create
+  end
+  resources :users, only: :show
   get 'prototypes/index'
   root to: "prototypes#index"
 end
